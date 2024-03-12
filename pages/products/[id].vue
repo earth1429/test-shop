4b92167e48ba3b97f18e30d88ab315e24ecc58c5
earth1429 <roboza000@gmail.com>
2024-03-12 18:44:38 +0700

    update Image Gallery

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -12,26 +12,30 @@
                 <img :src=product.thumbnail>
                 <div class="detail">
 
-                    <span class="text-4xl pt-2 font-bold">Name: </span>
+                    <span class="pt-2 text-4xl font-bold">Name: </span>
                     <span> {{ product.title }}</span><br>
 
-                    <span class="text-2xl pt-2 font-bold">Price: $</span>
+                    <span class="pt-2 text-2xl font-bold">Price: $</span>
                     <span>{{ product.price }}</span><br>
 
-                    <span class="text-2xl pt-2 font-bold">Category: ></span>
+                    <span class="pt-2 text-2xl font-bold">Category: ></span>
                     <span>{{ product.category }}</span><br>
 
-                    <span class="text-xl pt-2 font-bold">Brand: </span>
+                    <span class="pt-2 text-xl font-bold">Brand: </span>
                     <span>{{ product.brand }}</span><br>
 
-                    <span class="text-xl pt-2 font-bold">Discount: </span>
+                    <span class="pt-2 text-xl font-bold">Discount: </span>
                     <span>{{ product.discountPercentage }}</span><br>
 
-                    <span class="text-xl pt-2 font-bold">Rating: </span>
+                    <span class="pt-2 text-xl font-bold">Rating: </span>
                     <span>{{ product.rating }}</span><br>
                     
-                    <span class="text-xl pt-2 font-bold">Description: </span>
+                    <span class="pt-2 text-xl font-bold">Description: </span>
                     <span>{{ product.description }}</span><br>
+
+                    <span class="pt-2 text-xl font-bold">Image: </span>
+                    <img :src="item" v-for="item in product.images">{{ item }}</img><br>
+                    
                 </div>
             </div>
         </div>
